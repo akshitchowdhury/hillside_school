98ff546b940d37b23497d0dc401b3cbf8d5ca89f
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-07-22 15:55:30 +0530

    footer dump3

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -44,16 +44,18 @@ const Footer = () => {
             <div className="w-full sm:w-1/3 mb-8 sm:mb-0">
               <div className="footer-c-widget h-white list-widget text-white">
                 <h3 className="text-lg font-semibold mb-4">QUICK LINKS</h3>
-                <ul className='text-left px-20'>
-  <li><a href="javascript:void(0);" className="block mb-2">Home</a></li>
-  <li><a href="javascript:void(0);" className="block mb-2">About Us</a></li>
-  <li><a href="javascript:void(0);" className="block mb-2">Group of Institutions</a></li>
-  <li><a href="javascript:void(0);" className="block mb-2">Research</a></li>
-  <li><a href="javascript:void(0);" className="block mb-2">Alumni Association</a></li>
-  <li><a href="javascript:void(0);" className="block mb-2">Admission</a></li>
-  <li><a href="javascript:void(0);" className="block mb-2">Admission 2023-24</a></li>
-  <li><a href="javascript:void(0);" className="block mb-2">Contact</a></li>
+                <ul className="text-left px-20">
+  <li className="relative"><a href="javascript:void(0);" className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">Home</a></li>
+  <li className="relative"><a href="javascript:void(0);" className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">About Us</a></li>
+  <li className="relative"><a href="javascript:void(0);" className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">Group of Institutions</a></li>
+  <li className="relative"><a href="javascript:void(0);" className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">Research</a></li>
+  <li className="relative"><a href="javascript:void(0);" className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">Alumni Association</a></li>
+  
+  <li className="relative"><a href="javascript:void(0);" className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">Admission 2023-24</a></li>
+  <li className="relative"><a href="javascript:void(0);" className="block mb-2 after:content-[''] after:absolute after:w-10 after:h-0.5 after:bg-amber-400 after:left-0 after:bottom-0 after:scale-x-0 hover:after:scale-x-100 after:origin-left after:transition-transform after:duration-300">Contact</a></li>
 </ul>
+
+
 
               </div>
             </div>
@@ -61,16 +63,16 @@ const Footer = () => {
             {/* Column */}
             <div className="w-full sm:w-1/3 mb-8 sm:mb-0">
               <div className="footer-c-widget h-white address-list text-white">
-                <h3 className="text-lg md:text-left font-semibold mb-4">CONTACT US</h3>
-                <ul>
+                <h3 className="text-lg md:text-left  font-semibold mb-4">CONTACT US</h3>
+                <ul className='text-left'>
                   <li className="flex items-start mb-4"><FontAwesomeIcon icon={faMapMarkerAlt} className='text-amber-400 text-xl px-4'/># 9, Raghuvanahalli, Gubbalala Cross, Kanakapura Main Road, Bangalore-560062, Karnataka, India.</li>
                   <hr className="border-gray-700 mb-4" />
-                  <h5 className="text-md md:text-left font-semibold mb-2">ADMISSION</h5>
+                  <h5 className="text-md text-center md:text-left font-semibold mb-2">ADMISSION</h5>
                   <li className="flex items-start mb-4"><FontAwesomeIcon icon={faMobileAlt} className='text-amber-400 text-xl px-4'/> 080 655 87775, 080 655 87776, 098 459 56633, 097 428 03553, 099 005 42900</li>
                   <hr className="border-gray-700 mb-4" />
-                  <li className="flex items-start mb-4"><FontAwesomeIcon icon={faFileAlt} className='text-amber-400 text-xl px-4'/> <a href="brochure/Hillside-Academy-Brochure.pdf" target="_blank" className="text-white">Download Brochure</a></li>
+                  <li className="flex items-start mb-4"><FontAwesomeIcon icon={faFileAlt} className='text-amber-400 text-xl px-4 '/> <a href="brochure/Hillside-Academy-Brochure.pdf" target="_blank" className="text-white text-center">Download Brochure</a></li>
                   <hr className="border-gray-700 mb-4" />
-                  <li className="flex items-start"><FontAwesomeIcon icon={faFileArchive} className='text-amber-400 text-xl px-4'/> <a href="privacy-policy.html" className="text-white">Privacy Policy</a></li>
+                  <li className="flex items-start"><FontAwesomeIcon icon={faFileArchive} className='text-amber-400 text-xl px-4'/> <a href="privacy-policy.html" className="text-white text-center">Privacy Policy</a></li>
                 </ul>
               </div>
             </div>
