d19d96a0051cd965756bb29af3c406f6a3e4a2de
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-08-02 12:15:39 +0530

    AboutPart2

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 // import './About.css'
-import aboutBg from "../../assets/aboutBg.jpg"
 import AboutText from './AboutText'
+import AboutHero from './AboutHero'
+import AboutPointers from './AboutPointers'
 const About = () => {
   return (
     <>
     <div className='AboutBg'>
         
-    <div className='relative w-full h-[400px]'>
-  <img src={aboutBg} className='object-cover w-full h-full mix-blend-multiply' alt="Background Image"/>
-  <div className='absolute inset-0 bg-blue-400 opacity-40'></div>
-</div>
+    <AboutHero/>
 
-        <AboutText/>
+    <AboutText/>
+
+
+    <AboutPointers/>
       </div>
       </>
   )
---
+++ src/component/About/AboutPointers.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FaBook, FaRunning, FaLaptop, FaChild } from 'react-icons/fa';
+
+const features = [
+  {
+    icon: <FaBook className="text-4xl mb-4" />,
+    title: 'Comprehensive Curriculum',
+    description: 'Offering a well-rounded education that emphasizes both academic excellence and creative development.'
+  },
+  {
+    icon: <FaRunning className="text-4xl mb-4" />,
+    title: 'State-of-the-Art Sports Facilities',
+    description: 'Equipped with modern sports areas and dedicated coaches to foster physical fitness and teamwork.'
+  },
+  {
+    icon: <FaLaptop className="text-4xl mb-4" />,
+    title: 'Innovative Learning Environment',
+    description: 'Utilizing the latest technology and teaching methods to create an engaging and interactive classroom experience.'
+  },
+  {
+    icon: <FaChild className="text-4xl mb-4" />,
+    title: 'Holistic Development Programs',
+    description: 'Focused on nurturing emotional, social, and intellectual growth through extracurricular activities and personalized support.'
+  },
+];
+
+const AboutPointers = () => {
+  return (
+    <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {features.map((feature, index) => (
+          <div 
+            key={index} 
+            className={`p-6 shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300 ${
+              index % 2 === 0 ? 'bg-white text-gray-800 border-2 border-indigo-950' : ' bg-indigo-900 text-white'
+            }`}
+          >
+            <div className="flex flex-col items-center text-center">
+              <div className={index % 2 === 0 ? 'text-indigo-500' : 'text-yellow-500'}>
+                {feature.icon}
+              </div>
+              <h3 className="text-xl font-semibold mt-4">{feature.title}</h3>
+              <p className="mt-2 text-sm text-left">{feature.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AboutPointers;
