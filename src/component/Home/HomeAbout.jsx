895865283bc62ae562637fe78a8801f3862dafb2
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-08-23 14:35:58 +0530

    Text changes2

--- src/component/Home/HomeAbout.jsx
+++ src/component/Home/HomeAbout.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import aboutmain from "../../assets/hom1.jpg";
-import HomeCount from '../Home/HomeCount/HomeCount';
+import React from "react";
+import aboutmain from "../../assets/homeExtra.jpg";
+import HomeCount from "../Home/HomeCount/HomeCount";
 
 // const HomeAbout = () => {
 //   return (
@@ -30,12 +30,12 @@ import HomeCount from '../Home/HomeCount/HomeCount';
 //       <div className="flex flex-col-reverse lg:flex-row items-start">
 //         {/* Left side: Title and Paragraph */}
 //         <div className="lg:w-1/2 w-full lg:pr-3 ">
-        
+
 //           <p className="text-lg text-left text-gray-600">
-//           Hillside Group of Institutions started back in 1996 by Bheemachandra Education Trust. 
-//           Hillside Group has various educational institutions ranging from Kindergarten to PG courses in and around Bangalore City. Started with healthcare education, basic schools and added more and more courses in the field of nursing, Pharmacy College, business management and commerce. 
+//           Hillside Group of Institutions started back in 1996 by Bheemachandra Education Trust.
+//           Hillside Group has various educational institutions ranging from Kindergarten to PG courses in and around Bangalore City. Started with healthcare education, basic schools and added more and more courses in the field of nursing, Pharmacy College, business management and commerce.
 //           Hillside Group of Institutions is a distinguished educational institution, combining excellence with innovation.
-           
+
 //           </p>
 //         </div>
 //         {/* Right side: Image */}
@@ -51,48 +51,57 @@ import HomeCount from '../Home/HomeCount/HomeCount';
 const HomeAbout = () => {
   return (
     <>
-    <div className=' bg-blue-950 '>
-    <div className="container mx-auto px-4 sm:px-6 lg:px-12">
-      <div className="flex flex-col-reverse lg:flex-row items-start">
-        {/* Left side: Title and Paragraph */}
-        <div className="lg:w-2/3 w-full lg:pr-3 ">
-        <h3 className='text-amber-400 text-lg text-left my-10'>How it started for us</h3>
-          <h2 className="text-4xl lg:text-8xl md:text-7xl font-normal text-left text-zinc-50 mb-4">
-            Hillside School for Children
-          </h2>
-          {/* <p className="text-sm text-left text-gray-600">
+      <div className=" bg-blue-950 ">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-12">
+          <div className="flex flex-col-reverse lg:flex-row items-start">
+            {/* Left side: Title and Paragraph */}
+            <div className="lg:w-2/3 w-full lg:pr-3 ">
+              <h3 className="text-amber-400 text-lg text-left my-10">
+                How it started for us
+              </h3>
+              <h2 className="text-4xl lg:text-8xl md:text-7xl font-normal text-left text-zinc-50 mb-4">
+                Hillside School for Children
+              </h2>
+              {/* <p className="text-sm text-left text-gray-600">
             Hillside School for Children is dedicated to nurturing young minds with excellence in education, state-of-the-art facilities, and a supportive environment.
           </p> */}
+            </div>
+            {/* Right side: Image */}
+            <div className="lg:w-1/2 py-7 w-full">
+              <img
+                src={aboutmain}
+                alt="Hillside School"
+                className="w-full h-auto rounded-lg shadow-lg"
+              />
+            </div>
+          </div>
         </div>
-        {/* Right side: Image */}
-        <div className="lg:w-1/2 py-7 w-full">
-          <img src={aboutmain} alt="Hillside School" className="w-full h-auto rounded-lg shadow-lg" />
-        </div>
-      </div>
-    </div>
 
-    {/* part 2 */}
-    <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-12">
-      <div className="flex flex-col-reverse lg:flex-row items-start">
-        {/* Left side: Title and Paragraph */}
-        <div className="lg:w-1/2 w-full lg:pr-3 ">
-        
-          <p className="text-lg text-justify text-gray-50">
-          Hillside Group of Institutions started back in 1996 by Bheemachandra Education Trust. 
-          Hillside Group has various educational institutions ranging from Kindergarten to PG courses in and around Bangalore City. Started with healthcare education, basic schools and added more and more courses in the field of nursing, Pharmacy College, business management and commerce. 
-          Hillside Group of Institutions is a distinguished educational institution, combining excellence with innovation.
-           
-          </p>
-        </div>
-        {/* Right side: Image */}
-        <div className="lg:w-1/2 w-full">
-          <HomeCount/>
+        {/* part 2 */}
+        <div className="container mx-auto py-12 px-4 sm:px-6 lg:px-12">
+          <div className="flex flex-col-reverse lg:flex-row items-start">
+            {/* Left side: Title and Paragraph */}
+            <div className="lg:w-1/2 w-full lg:pr-3 ">
+              <p className="text-lg text-justify text-gray-50">
+                <span className = "text-4xl text-amber-400 font-semibold">Hillside Group of Institutions </span>started back in 1996 by
+                Bheemachandra Education Trust. Hillside Group has various
+                educational institutions ranging from Kindergarten to PG courses
+                in and around Bangalore City. Started with healthcare education,
+                basic schools and added more and more courses in the field of
+                nursing, Pharmacy College, business management and commerce.
+                Hillside Group of Institutions is a distinguished educational
+                institution, combining excellence with innovation.
+              </p>
+            </div>
+            {/* Right side: Image */}
+            <div className="lg:w-1/2 w-full">
+              <HomeCount />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    </div>
     </>
   );
-}
+};
 
 export default HomeAbout;
--- src/component/Nav/NavHome/NavHome.jsx
+++ src/component/Nav/NavHome/NavHome.jsx
@@ -15,7 +15,7 @@ const NavHome = () => {
             </span>
             <h3 className="text-xl text-white font-semibold mb-4">VISION AND MISSION</h3>
             <p className="text-white text-sm mb-4 text-left h-20">
-              The mission of Hillside Group of Institutions is to help see the students their career prospective and act to get their ultimate goal. Hillside Academy is one of its kind...
+              The mission of Bethel English high School is to help see the students their career prospective and act to get their ultimate goal. Hillside Academy is one of its kind...
             </p>
             <a
   className="bg-amber-400 text-white text-2xl rounded-full 
@@ -33,8 +33,8 @@ const NavHome = () => {
               <FontAwesomeIcon icon={faScroll} className="text-5xl text-amber-400" />
             </span>
             <h3 className="text-xl text-white font-semibold mb-4">HISTORY</h3>
-            <p className="text-white text-sm h-20 mb-4 text-left">
-              Hillside Academy started back in 1996 by Bheemachandra Education Trust. Hillside Group has various educational institutions ranging from Kindergarten...
+            <p className="text-white text-sm h-20 mb-4 text-justify">
+            Bethel English high School started back in 1996 by Bheemachandra Education Trust. Hillside Group has various educational institutions ranging from Kindergarten...
             </p>
             <a
   className="bg-amber-400 text-white text-2xl rounded-full 
@@ -52,8 +52,8 @@ const NavHome = () => {
               <FontAwesomeIcon icon={faSchoolFlag} className="text-5xl text-amber-400" />
             </span>
             <h3 className="text-xl text-white font-semibold mb-4">ADMINISTRATION</h3>
-            <p className="text-white text-sm mb-4 text-left h-20">
-              The infrastructure and facilities of Hillside Academy consists of spread-out campus alongside various amenities in the form of library, computer center...
+            <p className="text-white text-sm mb-4 text-justify h-20">
+              The infrastructure and facilities of Bethel English high School consists of spread-out campus alongside various amenities in the form of library, computer center...
             </p>
             <a
   className="bg-amber-400 text-white text-2xl rounded-full 
